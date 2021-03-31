4dd897897340fdcf7890023bad00c18b20a15b58
PratikGarai <pratikgarai0208@gmail.com>
2021-03-31 16:23:40 +0530

    first component

---
+++ src/components/Searchbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="searchbar__container">
+        <input type="text" name="search" placeholder="Search..." />
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name : "Searchbar"
+    }
+</script>
