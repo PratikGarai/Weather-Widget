8f11dadc0fa2c10be1d2d8a573ba1258f110c39e
PratikGarai <pratikgarai0208@gmail.com>
2021-04-21 22:11:19 +0530

    added and styled components with dummy data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="main">
     <main>
+
       <div class="search">
         <input type="text" class="search__input" placeholder="Search... "/>
+      </div>
+
+      <div class="location">
+        <div class="location__inner">
+          <div class="location__name">Kolkata</div>
+          <div class="location__date">Sunday 21 April 2021</div>
+        </div>
+      </div>
+
+      <div class="weather">
+        <div class="weather__temp">25ºC</div>
+        <div class="weather__climate">Rain</div>
       </div>
     </main>
   </div>
@@ -74,6 +87,49 @@ export default {
   box-shadow : 0px 0px 16px rgba(0,0,0,0.25);
   border-radius : 16px 0px 16px 0px;
   background-color : rgba(255, 255, 255, 0.75);
+}
+
+.location__name {
+  color : #fff;
+  font-size : 32px;
+  font-weight : 500;
+  text-align : center;
+  text-shadow : 1px 3px rgba(0,0,0,0.25);
+}
+
+.location__date {
+  color : #fff;
+  font-size : 20px;
+  font-weight : 300;
+  font-style : italic;
+  text-align : center;
+}
+
+.weather {
+  text-align : center;
+}
+
+.weather__temp{
+  display : inline-block;
+  padding : 10px 25px;
+  color : #fff;
+  font-size : 102px;
+  font-weight : 900;
+
+  text-shadow : 3px 6px rgba(0,0,0,0.25);
+  background-color : rgba(255,255,255,0.25);
+  border-radius : 16px;
+  margin : 30px 0;
+
+  box-shadow : 3px 6px rgba(0,0,0,0.25);
+}
+
+.weather__climate{
+  color : #fff;
+  font-size : 48px;
+  font-weight : 700;
+  font-style : italic;
+  text-shadow : 3px 6px rgba(0,0,0,0.25);
 }
 
 </style>
